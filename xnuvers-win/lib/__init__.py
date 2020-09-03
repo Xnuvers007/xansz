9b5cffe69dd8533b3b1a327ef9a47fa315f39d21
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2020-09-04 03:14:00 +0700

    Add files via upload

---
+++ xnuvers-win/lib/__init__.py
@@ -0,0 +1,7 @@
+#
+# Copyright (c) 2020 by Xnuvers <xnuversh1kar4@gmail.com>
+#
+# See 'LICENSE' for copying
+#
+
+
---
+++ xnuvers-win/modules/email.py
@@ -0,0 +1,25 @@
+#
+# email.py - Winner module - send email with HTML content.
+#
+# Copyright (c) 2020 Xnuvers <xnuversh1kar4@gmail.com>
+#
+# See 'LICENSE' for module copying
+#
+
+import smtplib
+
+# Module global configs
+MODULE_AUTHOR = "Xnuvers <xnuversh1kar4@gmail.com>"
+MODULE_LICENSE = "GPLv3"
+MODULE_VERSION = "0.1"
+MODULE_DATE = "22-01-2016"
+# Keep it short
+MODULE_DE   = "Send an email with HTML content."
+
+class Email(object):
+    """ Send HTML email """
+    def __init__(self):
+        pass
+
+def main():
+    pass
